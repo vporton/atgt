731cd059fa26233670ac3c3e589fbb61a8e6270c
Victor Porton <porton.victor@gmail.com>
2026-01-27 10:02:37 +0200

    AI fixes

--- Atgt/funcoids.lean
+++ Atgt/funcoids.lean
@@ -10,19 +10,24 @@ def is_least{u}(s: PartialOrder u)(a: u) := ∀ x, a ≤ x
 def meet{u}(s: PartialOrder u)(a b: u) := ∃ c, c ≤ a ∧ c ≤ b ∧ ¬ (is_least s c)
 
 /- TODO: Should be in `PartialOrder`, instead. -/
-theorem meet_as_inf{u}(s: SemilatticeInf u)(a b: u)
-    : (meet s.toPartialOrder a b) ↔ ¬ (is_least s.toPartialOrder (a ⊓ b))
-    := Iff.intro
-        (λ h: (meet s.toPartialOrder a b) ↦
-        show ¬ (is_least s.toPartialOrder (a ⊓ b))
-        from
-            have conj := h.elim
-            have m: (a ⊓ b) ≤ a ∧ (a ⊓ b) ≤ b := And.intro inf_le_left inf_le_right
-            have m2: ∀ c, c ≤ a → c ≤ b → c ≤ a ⊓ b := λ c ↦ (le_inf : c ≤ a → c ≤ b → c ≤ a ⊓ b)
-            suffices ¬ ∀ x, a ⊓ b ≤ x from by simp[is_least, meet]
-            show ¬ ∀ x, a ⊓ b ≤ x from _
-        )
-        sorry
+theorem meet_as_inf {u}
+  (s : SemilatticeInf u) (a b : u) :
+  meet s.toPartialOrder a b ↔
+  ¬ is_least s.toPartialOrder (a ⊓ b) :=
+by
+  constructor
+  · intro h
+    rcases h with ⟨c, hc₁, hc₂, hnot⟩
+    intro hleast
+    apply hnot
+    intro x
+    have := hleast x
+    have hcab : c ≤ a ⊓ b :=
+      le_inf hc₁ hc₂
+    exact le_trans hcab this
+  · intro h
+    refine ⟨a ⊓ b, inf_le_left, inf_le_right, ?_⟩
+    exact h
 
 structure PointfreeFuncoid {u v}(a: PartialOrder u)(b: PartialOrder v) where
     fwd : u → v
@@ -40,11 +45,15 @@ instance inv {u v}
 theorem inv_inv_funcoid{u v}(a: PartialOrder u)(b: PartialOrder v)(f: PointfreeFuncoid a b) :
     inv b a (inv a b f) = f := by simp[inv]
 
-instance PointfreeFuncoid.on_semilattice_inf(a: SemilatticeInf u)(b: SemilatticeInf u)
-    : PointfreeFuncoid a.toPartialOrder b.toPartialOrder where
-    fwd := f.fwd
-    bwd := f.bwd
-    rev := ∀ x: a, y: b, is_least (fwd x) ⊓ y ↔ is_least (bwd y) ⊓ x := sorry
+instance PointfreeFuncoid.on_semilattice_inf
+  (a : SemilatticeInf u) (b : SemilatticeInf v)
+  (f : PointfreeFuncoid a.toPartialOrder b.toPartialOrder) :
+  PointfreeFuncoid a.toPartialOrder b.toPartialOrder :=
+f
 
-instance PointfreeFuncoid.poset{a b: poset} : poset (PointfreeFuncoid a b) where
-    { le := ∀ x, f.fwd x ≤ g.fwd x ∧ ∀ y, g.bwd y ≤ f.bwd y } := sorry
+instance PointfreeFuncoid.instLE
+  {u v} (a : PartialOrder u) (b : PartialOrder v) :
+  LE (PointfreeFuncoid a b) :=
+⟨fun f g =>
+  (∀ x, f.fwd x ≤ g.fwd x) ∧
+  (∀ y, g.bwd y ≤ f.bwd y)⟩
