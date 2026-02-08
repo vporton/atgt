51cc9c0e8b85161f16c0e6f621ba6a571855ec5f
Victor Porton <porton.victor@gmail.com>
2026-02-08 14:03:29 +0200

    improved by AI

--- atgt/Poset.lean
+++ atgt/Poset.lean
@@ -6,20 +6,20 @@ instance {u} : Coe (SemilatticeInf u) (PartialOrder u) where
     coe s := s.toPartialOrder
 
 /- TODO: Should be in `Ordset`, instead. -/
-def is_least{u}(s: PartialOrder u)(a: u) := ∀ x, a ≤ x
+def is_least {α : Type u} [PartialOrder α] (a : α) := ∀ x, a ≤ x
 
 /- TODO: Should be in `Ordset`, instead. -/
-def meet{u}(s: PartialOrder u)(a b: u) := ∃ c, c ≤ a ∧ c ≤ b ∧ ¬ (is_least s c)
+def meet {α : Type u} [PartialOrder α] (a b : α) := ∃ c, c ≤ a ∧ c ≤ b ∧ ¬ (is_least c)
 
-theorem meet_comm{u}(s: PartialOrder u)(a b: u) : meet s a b ↔ meet s b a := by
+theorem meet_comm {α : Type u} [PartialOrder α] (a b : α) : meet a b ↔ meet b a := by
     simp [meet]
     tauto
 
 /- TODO: Should be in `Ordset`, instead. -/
-theorem meet_as_inf {u}
-  (s : SemilatticeInf u) (a b : u) :
-  @meet _ s a b ↔
-  ¬ @is_least u s (a ⊓ b) :=
+theorem meet_as_inf {α : Type u}
+  [s : SemilatticeInf α] (a b : α) :
+  meet a b ↔
+  ¬ is_least (a ⊓ b) :=
 by
   constructor
   · intro h
@@ -35,4 +35,8 @@ by
     refine ⟨a ⊓ b, inf_le_left, inf_le_right, ?_⟩
     exact h
 
--- def separator {s: PartialOrder u} (a b: Type u) := { x: a | ¬ meet s x b }
+def separator {α : Type u} [PartialOrder α] (a : α) := { x : α | ¬ meet x a }
+
+prefix:80 "⋆" => separator
+
+def Ordset.is_separable {α : Type u} [PartialOrder α] (a b : α) := a ≠ b → separator a ≠ separator b
