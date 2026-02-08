9fd92caf6e8588a2b1b58fe4b15451482e5e3352
Victor Porton <porton.victor@gmail.com>
2026-02-08 15:55:32 +0200

    Filtrator.separable_core

--- atgt/Filtrator.lean
+++ atgt/Filtrator.lean
@@ -1,8 +1,12 @@
 import Mathlib.Data.Ordmap.Ordset
+import atgt.Poset
 
 structure Filtrator (α : Type u) where
   order : PartialOrder α
   subset : Set α
 
-def Filtrator.suborder (α : Type u) (F : Filtrator α) : PartialOrder F.subset :=
+def Filtrator.suborder {α : Type u} (F : Filtrator α) : PartialOrder F.subset :=
   @Subtype.partialOrder α F.order F.subset
+
+def Filtrator.separable_core {α : Type u} (F : Filtrator α) : Prop :=
+  ∀ a b : α, @base_separator α F.subset F.order a = @base_separator α F.subset F.order b → a = b
