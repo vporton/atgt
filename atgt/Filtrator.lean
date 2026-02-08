ce534e6cd6f0f28fb7a4a292442ac629b3829f3f
Victor Porton <porton.victor@gmail.com>
2026-02-08 15:46:58 +0200

    fix bug

--- atgt/Filtrator.lean
+++ atgt/Filtrator.lean
@@ -4,5 +4,5 @@ structure Filtrator (α : Type u) where
   order : PartialOrder α
   subset : Set α
 
-def Filtrator.suborder (α : Type u) (F : Filtrator α) : PartialOrder _ :=
+def Filtrator.suborder (α : Type u) (F : Filtrator α) : PartialOrder F.subset :=
   @Subtype.partialOrder α F.order F.subset
