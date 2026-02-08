c528ac7f23136c5388432798ee96636d7020a5eb
Victor Porton <porton.victor@gmail.com>
2026-02-08 15:46:00 +0200

    Filtrator.suborder

--- atgt/Filtrator.lean
+++ atgt/Filtrator.lean
@@ -3,3 +3,6 @@ import Mathlib.Data.Ordmap.Ordset
 structure Filtrator (α : Type u) where
   order : PartialOrder α
   subset : Set α
+
+def Filtrator.suborder (α : Type u) (F : Filtrator α) : PartialOrder _ :=
+  @Subtype.partialOrder α F.order F.subset
