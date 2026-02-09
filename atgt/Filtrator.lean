92e4dcc90d16b550f9503653d553836381218907
Victor Porton <porton.victor@gmail.com>
2026-02-09 04:50:21 +0200

    Filtered and up_determined equivalency

--- atgt/Filtrator.lean
+++ atgt/Filtrator.lean
@@ -8,8 +8,6 @@ class Filtrator (α : Type*) extends PartialOrder α where
 
 export Filtrator (subset)
 
-variable {α : Type*} [Filtrator α]
-
 def Filtrator.suporder {α : Type*} [Filtrator α] : PartialOrder α := inferInstance
 
 def Filtrator.suborder {α : Type*} [Filtrator α] : PartialOrder (subset : Set α) :=
@@ -17,11 +15,32 @@ def Filtrator.suborder {α : Type*} [Filtrator α] : PartialOrder (subset : Set 
 
 def Filtrator.supset {α : Type u} [Filtrator α] := α
 
-variable {α : Type*} [Filtrator α]
+-- variable {α : Type*} [Filtrator α]
 
 def Filtrator.up {α : Type u} [Filtrator α] (x: α) := { y ∈ subset | x ≤ y }
 
-/-- A filtrator is core-determined if every element is the infimum of its core upper set. -/
-/- TODO: In the book, I call it differently. -/
-def Filtrator.core_determined {α : Type u} [Filtrator α] : Prop :=
-  ∀ x : α, IsGLB (Filtrator.up x) x
+class Filtrator.Filtered (α : Type u) [Filtrator α] : Prop where
+  is_filtered : ∀ x y : α, up x ⊆ up y → y ≤ x
+
+/-- A filtrator is up-determined if every element is the infimum of its core upper set. -/
+theorem Filtrator.up_determined_iff_filtered {α : Type u} [Filtrator α] :
+  Filtrator.Filtered α ↔ ∀ x : α, IsGLB (Filtrator.up x) x := by
+  constructor
+  · intro h x
+    constructor
+    · intro y hy
+      exact hy.2
+    · intro y hy
+      apply h.is_filtered
+      intro z hz
+      exact ⟨hz.1, hy hz⟩
+  · intro h
+    constructor
+    intro x y h_subs
+    apply (h x).2
+    intro z hz
+    exact (h y).1 (h_subs hz)
+
+/- For simplicity, I define it only for semilattices. In the book it's more general. -/
+def Filtrator.binary_meet_closed {α : Type u} [Filtrator α] [SemilatticeInf α] : Prop :=
+  ∀ x y : α, x ∈ subset → y ∈ subset → x ⊓ y ∈ subset
