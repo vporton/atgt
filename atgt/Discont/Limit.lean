10fc2c0411b735096d522c2809db67526915eac7
Victor Porton <porton.victor@gmail.com>
2026-03-01 18:45:55 +0200

    typo in an identifier

--- atgt/Discont/Limit.lean
+++ atgt/Discont/Limit.lean
@@ -10,5 +10,5 @@ def IsFuncoidLimit {α: Type u} {β: Type v} (d: Funcoid α β) (F: Filtrator.Fi
 def IsBinaryRelationLimit {α: Type u} {β: Type v} (d: α → β → Prop) (F: Filtrator.FilterOnPowerset β) (x: α) :=
   IsFuncoidLimit (principalFuncoid d) F x
 
-def IsFunctiondLimit {α: Type u} {β: Type v} (d: α → β) (F: Filtrator.FilterOnPowerset β) (x: α) :=
+def IsFunctionLimit {α: Type u} {β: Type v} (d: α → β) (F: Filtrator.FilterOnPowerset β) (x: α) :=
   IsFuncoidLimit (principalFuncoidOfFunction d) F x
