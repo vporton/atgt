cffea78e1bc20e84085264c9ea9a606a3821a261
Victor Porton <porton.victor@gmail.com>
2026-03-05 17:24:05 +0200

    restored atgt.AlternativePrimaryFiltrators

--- atgt.lean
+++ atgt.lean
@@ -3,7 +3,7 @@
 import atgt.Filtrator.GeneralizedFilterBase
 import atgt.Filtrator.Powerset
 import atgt.Filtrator.AdvancedProperties
--- import atgt.AlternativePrimaryFiltrators
+import atgt.AlternativePrimaryFiltrators
 -- import atgt.PointfreeFuncoid
 -- import atgt.Funcoid
 -- import atgt.Funcoid.Topology
